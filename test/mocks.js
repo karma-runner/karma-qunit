e39f3c4d56d52eaed480c3953f929ab724f6cb54
Vojta Jina <vojta.jina@gmail.com>
2013-01-26 20:05:48 -0800

    Initial commit, extracted from https://github.com/testacular/testacular

---
+++ test/mocks.js
@@ -0,0 +1,41 @@
+var Emitter = function() {
+  var listeners = {};
+
+  this.on = function(event, fn) {
+    if (!listeners[event]) {
+      listeners[event] = [];
+    }
+
+    listeners[event].push(fn);
+  };
+
+  this.done = function(fn) {
+    this.on("done", fn);
+  };
+
+  this.testStart = function(fn) {
+    this.on("testStart", fn);
+  };
+
+  this.testDone = function(fn) {
+    this.on("testDone", fn);
+  };
+
+  this.log = function(fn) {
+    this.on("log", fn);
+  };
+
+  this.emit = function(event) {
+    var eventListeners = listeners[event];
+
+    if (!eventListeners) return;
+
+    var i = 0;
+    while (i < eventListeners.length) {
+      eventListeners[i].apply(null, Array.prototype.slice.call(arguments, 1));
+      i++;
+    }
+  };
+};
+
+var MockSocket = Emitter;
