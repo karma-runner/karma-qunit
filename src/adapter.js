4e90529f956b4e5e8df154e612f1555813f56e39
Kristof Neirynck <k.neirynck@gmail.com>
2013-05-02 20:39:18 +0200

    fix: Support integration with RequireJS qunit autostart and load must wait for all tests to be loaded with requirejs this means quite a bit later than window.onload
    closes #3

--- src/adapter.js
+++ src/adapter.js
@@ -1,55 +1,68 @@
+window.QUnit.config.autostart = false;
+
+(function removeEvent(elem, type, fn) {
+    if (elem.removeEventListener) {
+        elem.removeEventListener(type, fn, false);
+    } else {
+        elem.detachEvent('on' + type, fn);
+    }
+}(window, 'load', window.QUnit.load));
+
 var createQUnitStartFn = function (tc, runnerPassedIn) {
-	return function () {
-    var runner = runnerPassedIn || window.QUnit;
-		var totalNumberOfTest = 0;
-		var timer = null;
-    var testResult = {};
+    return function () {
+        var runner = runnerPassedIn || window.QUnit;
+        var totalNumberOfTest = 0;
+        var timer = null;
+        var testResult = {};
 
-		runner.done(function () {
-			tc.info({ total: totalNumberOfTest });
-			tc.complete({
-       coverage: window.__coverage__
-      });
-		});
+        runner.done(function () {
+            tc.info({ total: totalNumberOfTest });
+            tc.complete({
+                coverage: window.__coverage__
+            });
+        });
 
-		runner.testStart(function (test) {
-			totalNumberOfTest += 1;
-			timer = new Date().getTime();
-      testResult = { success: true, errors: [] };
-		});
+        runner.testStart(function (test) {
+            totalNumberOfTest += 1;
+            timer = new Date().getTime();
+            testResult = { success: true, errors: [] };
+        });
 
-    runner.log(function (details) {
-      if (!details.result) {
-        testResult.success = false;
-        testResult.errors.push(details.message + (details.source ? ('\n' + details.source) : ''));
-      }
-    });
+        runner.log(function (details) {
+            if (!details.result) {
+                testResult.success = false;
+                testResult.errors.push(details.message + (details.source ? ('\n' + details.source) : ''));
+            }
+        });
 
-		runner.testDone(function (test) {
-			var result = {
-				description: test.name,
-				suite: [test.module] || [],
-				success: testResult.success,
-				log: testResult.errors || [],
-				time: new Date().getTime() - timer
-			};
+        runner.testDone(function (test) {
+            var result = {
+                description: test.name,
+                suite: [test.module] || [],
+                success: testResult.success,
+                log: testResult.errors || [],
+                time: new Date().getTime() - timer
+            };
 
-			tc.result(result);
-		});
-	};
+            tc.result(result);
+        });
+
+        runner.load();
+        runner.start();
+    };
 };
 
 var createDumpFn = function (tc, serialize) {
-	return function () {
+    return function () {
 
-		var args = Array.prototype.slice.call(arguments, 0);
+        var args = Array.prototype.slice.call(arguments, 0);
 
-		if (serialize) {
-			for (var i = 0; i < args.length; i++) {
-				args[i] = serialize(args[i]);
-			}
-		}
+        if (serialize) {
+            for (var i = 0; i < args.length; i++) {
+                args[i] = serialize(args[i]);
+            }
+        }
 
-		tc.info({ dump: args });
-	};
+        tc.info({ dump: args });
+    };
 };
