6e0d709ae6ba0a1d6597facbbea9ee0ae0164d89
masubi <justin.masui@gmail.com>
2016-09-09 16:37:59 -0700

    fix adapter: linting issues

--- src/adapter.js
+++ src/adapter.js
@@ -103,8 +103,8 @@ function createQUnitStartFn (tc, runnerPassedIn) { // eslint-disable-line no-unu
         time: new Date().getTime() - timer
       }
 
-      if(result.description==="global failure"){
-        return;
+      if (result.description === 'global failure') {
+        return
       }
 
       tc.result(result)
